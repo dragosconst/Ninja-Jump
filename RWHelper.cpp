f5b436b2bf61136b5609e5cd6615e58065985b99
dragosconst <38582034+dragosconst@users.noreply.github.com>
2021-12-11 22:55:54 +0200

    improved eeprom

--- RWHelper.cpp
+++ RWHelper.cpp
@@ -6,73 +6,105 @@ const byte RWHelper::LCDbrightAddr = 1;
 const byte RWHelper::LEDbrightAddr = 2;
 const byte RWHelper::diffAddr = 3;
 const byte RWHelper::highScoreNumAddr = 4; // from 4 onwards the highscores will be stored
+const byte RWHelper::charLimit = 3;
 
 void RWHelper::clear() {
     for(int i = 0; i < 10; ++i) {
-        EEPROM.update(i, 0);
+        EEPROM.put(i, 0);
     }
 }
 
 void RWHelper::writeContrast(int value) {
-    EEPROM.update(RWHelper::contrastAddr, value);
+    EEPROM.put(RWHelper::contrastAddr, value);
 }
 
 void RWHelper::writeLCDBright(int value) {
-    EEPROM.update(RWHelper::LCDbrightAddr, value);
+    EEPROM.put(RWHelper::LCDbrightAddr, value);
 }
 
 void RWHelper::writeLEDBright(int value) {
-    EEPROM.update(RWHelper::LEDbrightAddr, value);
+    EEPROM.put(RWHelper::LEDbrightAddr, value);
 }
 
 void RWHelper::writeDiff(int value) {
-    EEPROM.update(RWHelper::diffAddr, value);
+    EEPROM.put(RWHelper::diffAddr, value);
 }
 
 void RWHelper::writeHighNum(int value) {
-    EEPROM.update(RWHelper::highScoreNumAddr, value);
+    EEPROM.put(RWHelper::highScoreNumAddr, value);
 }
 
-void RWHelper::writeHigh(int value) {
-    int n = EEPROM.read(RWHelper::highScoreNumAddr);
+void RWHelper::writeHigh(int value, char* name) {
+    byte highScoreSize = 1 + RWHelper::charLimit;
+    int n;
+    EEPROM.get(RWHelper::highScoreNumAddr, n);
     int oldValue = -1;
-    for(int i = 1; i <= n; ++i) {
-        int currentVal = EEPROM.read(RWHelper::highScoreNumAddr + i);
-        if(currentVal < value) {
-            EEPROM.update(RWHelper::highScoreNumAddr + i, value);
+    char oldName[RWHelper::charLimit];
+    for(int i = 0; i < n; ++i) {
+        int currentVal;
+        EEPROM.get(RWHelper::highScoreNumAddr + i * highScoreSize + 1, currentVal);
+        if(currentVal < value && oldValue == -1) {
+            EEPROM.put(RWHelper::highScoreNumAddr + i * highScoreSize + 1, value);
+            for(int j = 0; j < RWHelper::charLimit; ++j) {
+                EEPROM.get(RWHelper::highScoreNumAddr + i * highScoreSize + 2 + j, oldName[j]);
+                EEPROM.put(RWHelper::highScoreNumAddr + i * highScoreSize + 2 + j, name[j]);
+            }
             oldValue = currentVal;
+            continue;
         }
         if(oldValue > 0) { // shift back the other high scores
-            EEPROM.update(RWHelper::highScoreNumAddr + i, oldValue);
+            EEPROM.put(RWHelper::highScoreNumAddr + i * highScoreSize + 1, oldValue);
+            char aux;
+            for(int j = 0; j < RWHelper::charLimit; ++j) {
+                EEPROM.get(RWHelper::highScoreNumAddr + i * highScoreSize + 2 + j, aux);
+                EEPROM.put(RWHelper::highScoreNumAddr + i * highScoreSize + 2 + j, oldName[j]);
+                oldName[j] = aux;
+            }
             oldValue = currentVal;
         }
     }
 }
 
 int RWHelper::readContrast() {
-    return EEPROM.read(RWHelper::contrastAddr);
+    int retval;
+    EEPROM.get(RWHelper::contrastAddr, retval);
+    return retval;
 }
 
 int RWHelper::readLCDBright() {
-    return EEPROM.read(RWHelper::LCDbrightAddr);
+    int retval;
+    EEPROM.get(RWHelper::LCDbrightAddr, retval);
+    return retval;
 }
 
 int RWHelper::readLEDBright() {
-    return EEPROM.read(RWHelper::LEDbrightAddr);
+    int retval;
+    EEPROM.get(RWHelper::LEDbrightAddr, retval);
+    return retval;
 }
 
 int RWHelper::readDiff() {
-    return EEPROM.read(RWHelper::diffAddr);
+    int retval;
+    EEPROM.get(RWHelper::diffAddr, retval);
+    return retval;
 }
 
 int RWHelper::readHighNum() {
-    return EEPROM.read(RWHelper::highScoreNumAddr);
+    int retval;
+    EEPROM.get(RWHelper::highScoreNumAddr, retval);
+    return retval;
 }
 
-int RWHelper::readHigh(int which) {
-    int n = EEPROM.read(RWHelper::highScoreNumAddr);
+int RWHelper::readHigh(int which, char* writeHere) {
+    int retval;
+    int n;
+    EEPROM.get(RWHelper::highScoreNumAddr, n);
     if(which <= 0 || which > n)
         return -1; // invalid addresses
-    return EEPROM.read(RWHelper::highScoreNumAddr + which);
+    EEPROM.get(RWHelper::highScoreNumAddr + (which - 1) * (sizeof(int) + RWHelper::charLimit * sizeof(char)) + 1, retval);
+    for(byte j = 0; j < RWHelper::charLimit; ++j) {
+        EEPROM.get(RWHelper::highScoreNumAddr + (which - 1) * (sizeof(int) + RWHelper::charLimit * sizeof(char)) + 2 + j, writeHere[j]);
+    }
+    return retval;
 }
 
--- RWHelper.h
+++ RWHelper.h
@@ -9,6 +9,8 @@
  * This class will serve to perform R\W operations on the EEPROM, instead of doing them directly in code.
  * This way, I don't have to worry about hardcoding where I store my stuff elsewhere and I can also do some checks to minimize writing, without
  * getting more messy code elsewhere.
+ * 
+ * Highscores are stored like a (score, name) tuple, where score is a byte and name occupies charLimit bytes
  */
 class RWHelper {
 private:
@@ -17,6 +19,7 @@ private:
     static const byte LEDbrightAddr;
     static const byte diffAddr;
     static const byte highScoreNumAddr; // from 4 onwards the highscores will be stored
+    static const byte charLimit; // limit of chars in a name
 public:
     static void clear(); // !!!!! use this as rarely as possible, since it performs a total of 9 (at the moment) writes, which is a lot for arduino
     static void writeContrast(int value);
@@ -24,14 +27,14 @@ public:
     static void writeLEDBright(int value);
     static void writeDiff(int value);
     static void writeHighNum(int value);
-    static void writeHigh(int value);
+    static void writeHigh(int value, char* name);
 
     static int readContrast();
     static int readLCDBright();
     static int readLEDBright();
     static int readDiff();
     static int readHighNum();
-    static int readHigh(int which);
+    static int readHigh(int which, char* writeHere);
 };
 
 #endif
